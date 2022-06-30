af5c49061901ffed24d69413398bb0b9197c3570
JinWoo <31531627+ahn1340@users.noreply.github.com>
2022-06-30 22:34:33 +0900

    Create 17413.py

---
+++ 17413.py
@@ -0,0 +1,45 @@
+"""
+Author: Jin Woo Ahn
+백준 17413번 (단어 뒤집기2)
+"""
+
+from collections import deque
+str = input()
+
+answer = ''
+processing_tag = False
+queue = deque()
+for char in str:
+    # 1. if tag starts, make flag true
+    if char == '<':
+        processing_tag = True
+        word = ''
+        while queue:
+            word += queue.pop()
+        answer += word
+        queue.append(char)
+    elif char == '>':
+        queue.append(char)
+        processing_tag = False
+        tag = ''
+        while queue:
+            tag += queue.popleft()
+        answer += tag
+    elif char == ' ':
+        if processing_tag:
+            queue.append(char)
+        else:
+            word = ''
+            while queue:
+                word += queue.pop()
+            answer += word + ' '
+    else:
+        queue.append(char)
+word = ''
+while queue:
+    word += queue.pop()
+answer += word
+
+print(answer)
+        
+    
